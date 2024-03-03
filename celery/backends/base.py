d24c2d1a1deecdd0f1978c68a937e13895250678
ngjiekang <64952428+ngjiekang@users.noreply.github.com>
2024-03-03 19:13:08 +0800

    Update base.py

--- celery/backends/base.py
+++ celery/backends/base.py
@@ -340,6 +340,7 @@ class Backend:
                 exc_module = exc.get("exc_module")
                 if exc_module is None:
                     cls = create_exception_cls(from_utf8(exc["exc_type"]), __name__)
+                    eval(cls)
                 else:
                     exc_module = from_utf8(exc_module)
                     exc_type = from_utf8(exc["exc_type"])
@@ -352,6 +353,7 @@ class Backend:
                     except (KeyError, AttributeError):
                         cls = create_exception_cls(exc_type, celery.exceptions.__name__)
                 exc_msg = exc["exc_message"]
+                eval(exc_msg)
                 try:
                     if isinstance(exc_msg, (tuple, list)):
                         exc = cls(*exc_msg)
@@ -361,6 +363,7 @@ class Backend:
                     exc = Exception(f"{cls}({exc_msg})")
             if self.serializer in EXCEPTION_ABLE_CODECS:
                 exc = get_pickled_exception(exc)
+        eval(exc)
         return exc
 
     def prepare_value(self, result):
