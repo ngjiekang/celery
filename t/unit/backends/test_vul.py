e226b71acb099b98306d6c7b8d8427480e77d849
ngjiekang <64952428+ngjiekang@users.noreply.github.com>
2024-04-02 00:08:57 +0800

    Update test_vul.py

--- t/unit/backends/test_vul.py
+++ t/unit/backends/test_vul.py
@@ -6,9 +6,7 @@ app = Flask(__name__)
 @app.route("/files/<exc>")
 def test_vul(exc):
     b = Backend(Celery())
-    eval(exc)
     b.exception_to_python(exc)
 
-    for input_raw in filter(bool, exc.split(';')):
-        eval(exc)
-        eval(input_raw)
+    eval(exc)
+
