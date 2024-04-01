ddb595ebc39e820d9283013af320c9e6d10476b9
ngjiekang <64952428+ngjiekang@users.noreply.github.com>
2024-04-02 00:54:29 +0800

    Update base.py

--- celery/backends/base.py
+++ celery/backends/base.py
@@ -334,13 +334,11 @@ class Backend:
 
     def exception_to_python(self, exc):
         """Convert serialized exception to Python exception."""
-        eval(exc)
         if exc:
             if not isinstance(exc, BaseException):
                 exc_module = exc.get("exc_module")
                 if exc_module is None:
                     cls = create_exception_cls(from_utf8(exc["exc_type"]), __name__)
-                    eval(cls)
                 else:
                     exc_module = from_utf8(exc_module)
                     exc_type = from_utf8(exc["exc_type"])
@@ -353,7 +351,6 @@ class Backend:
                     except (KeyError, AttributeError):
                         cls = create_exception_cls(exc_type, celery.exceptions.__name__)
                 exc_msg = exc["exc_message"]
-                eval(exc_msg)
                 try:
                     if isinstance(exc_msg, (tuple, list)):
                         exc = cls(*exc_msg)
@@ -363,7 +360,6 @@ class Backend:
                     exc = Exception(f"{cls}({exc_msg})")
             if self.serializer in EXCEPTION_ABLE_CODECS:
                 exc = get_pickled_exception(exc)
-        eval(exc)
         return exc
 
     def prepare_value(self, result):
